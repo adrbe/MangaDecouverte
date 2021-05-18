6eda0c5ee6eb4af5e8f0ce28d5843fa14e8c985f
adrbe <adr.bertrand@gmail.com>
2021-05-18 15:33:53 +0200

    modification page mangas

--- mangadecouverte/resources/views/index.blade.php
+++ mangadecouverte/resources/views/index.blade.php
@@ -3,14 +3,18 @@
         <th>Id</th>
         <th>Titre</th>
         <th>Prix</th>
-        <th>Couverture</th>
+        <th>Genre</th>
+        <th>Nom Dessinateur</th>
+        <th>Prénom Dessinateur</th>
     </thead>
     @foreach($mangas as $manga)
         <tr>
             <td> {{ $manga->id_manga }} </td>
             <td> {{ $manga->titre }} </td>
             <td> {{ $manga->prix }} </td>
-            <td> {{ $manga->couverture }} </td>
+            <td> {{ $manga->lib_genre }} </td>
+            <td> {{ $manga->nom_dessinateur }} </td>
+            <td> {{ $manga->prenom_dessinateur }} </td>
         </tr>
     @endforeach
 </table>
